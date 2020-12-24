9941ea9cd83d49e571f87ad36130328f12af6460
Adil Hossain <38645490+Adilhossain227@users.noreply.github.com>
2020-12-24 20:18:26 +0600

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,11 @@
+import cv2
+x=1
+i=501
+new_width = 481
+new_height = 321
+dim = (new_width, new_height)
+while(x<i):
+	img = cv2.imread("test/"+str(test[x])+".jpg", cv2.IMREAD_UNCHANGED)
+	dresized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+	cv2.imwrite('trainx/'+str(test[x])+'.jpg', dresized)
+	x=x+1
