6ff2157aa8b66ff0110516cd958d1849ee658291
Adil Hossain <38645490+Adilhossain227@users.noreply.github.com>
2020-12-24 20:19:15 +0600

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ new_width = 481
 new_height = 321
 dim = (new_width, new_height)
 while(x<i):
-	img = cv2.imread("test/"+str(test[x])+".jpg", cv2.IMREAD_UNCHANGED)
+	img = cv2.imread("test/"+str(x)+".jpg", cv2.IMREAD_UNCHANGED)
 	dresized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-	cv2.imwrite('trainx/'+str(test[x])+'.jpg', dresized)
+	cv2.imwrite('trainx/'+str(x)+'.jpg', dresized)
 	x=x+1
